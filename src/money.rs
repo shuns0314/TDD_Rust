921245641ba108079dfc7e63f0ebe6b15f2e82d3
shuns0314 <shunsuke.naka0314@gmail.com>
2022-09-19 22:33:43 +0900

    add franc

--- src/money.rs
+++ src/money.rs
@@ -3,13 +3,18 @@ struct Doller {
     pub amount: i32
 }
 
-trait Money {
+#[derive(Debug,PartialEq)]
+struct Franc {
+    pub amount: i32
+}
+
+trait Money<T> {
     fn times(&mut self, multiplier: &'static i32) -> Self;
-    fn eqauls(&mut self, money: &Doller) -> bool;
+    fn eqauls(&mut self, money: &T) -> bool;
 }
 
 
-impl Money for Doller {
+impl Money<Doller> for Doller {
     fn times(&mut self, multiplier: &'static i32) -> Doller{
         return Doller {amount: self.amount * multiplier}
     }
@@ -18,10 +23,21 @@ impl Money for Doller {
     }
 }
 
+impl Money<Franc> for Franc {
+    fn times(&mut self, multiplier: &'static i32) -> Franc{
+        return Franc {amount: self.amount * multiplier}
+    }
+    fn eqauls(&mut self, money: &Franc) -> bool {
+        return money.amount == self.amount;
+    }
+}
+
 #[cfg(test)]
 mod test{
     use super::Money;
     use super::Doller;
+    use super::Franc;
+
     #[test]
     fn test_multiplication(){
         let mut doller = Doller {amount: 5};
@@ -34,5 +50,12 @@ mod test{
         let mut doller = Doller {amount: 5};
         assert!(doller.eqauls(&Doller {amount: 5}));
         assert!(!doller.eqauls(&Doller {amount: 6}));
+    }
+
+    #[test]
+    fn test_franc_multiplication(){
+        let mut franc = Franc {amount: 5};
+        assert_eq!(Franc {amount: 10}, franc.times(&2));
+        assert_eq!(Franc {amount: 15}, franc.times(&3));
     }
 }
